742aa3b9b116f946d1252a0091f7bb4f27cbb5ee
sankave <49546162+sankave@users.noreply.github.com>
2019-04-16 14:22:12 +0530

    Update MainApp.java

--- bank_customer_project/src/main/java/com/main/MainApp.java
+++ bank_customer_project/src/main/java/com/main/MainApp.java
@@ -3,7 +3,7 @@ package com.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+//comments
 @SpringBootApplication
 //@ComponentScan("com")
 public class MainApp {
